b9b0510f9c5b6759afdd1e7463fa7b2837ef53d8
Ryan Showman <rshowman@dpsolutions.com>
2026-05-26 16:48:22 -0400

    update tools.json and add two scripts for getting shutdown logs and resyncing time and auto time zone.

---
+++ scripts/get_shutdown_logs.ps1
@@ -0,0 +1,12 @@
+$LogPath = Join-Path $PSScriptRoot "ShutdownLog.txt"
+
+Get-WinEvent -FilterHashtable @{
+    LogName = 'System'
+    Id      = 41, 1074, 6006, 6605, 6008
+} |
+Format-List Id, LevelDisplayName, TimeCreated, Message |
+Out-String |
+ForEach-Object { $_.Trim() } |
+Out-File $LogPath
+
+Start-Process $LogPath
